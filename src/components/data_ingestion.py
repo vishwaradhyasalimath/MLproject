27c02d200fb8bc758458d0d251f9fcc951059867
vishwaradhya <vishwasalimath7@gmail.com>
2025-01-12 20:07:52 +0530

    model trainer

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -11,6 +11,9 @@ from dataclasses import dataclass
 from src.components.data_transformation import DataTransformation
 from src.components.data_transformation import DataTransformationConfig
 
+from src.components.model_trainer import ModelTrainerConfig
+from src.components.model_trainer import ModelTrainer
+
 @dataclass
 class DataIngestionConfig:
     train_data_path :str=os.path.join('artifacts','train.csv')
@@ -27,7 +30,8 @@ class DataIngestion:
             df=pd.read_csv('notebook\data\student.csv')
             logging.info('Read the dataset as dataframe')
 
-            os.makedirs(os.path.dirname(self.ingestion_config.train_data_path))
+            os.makedirs(os.path.dirname(self.ingestion_config.train_data_path), exist_ok=True)
+
 
             df.to_csv(self.ingestion_config.raw_data_path,index=False,header=True)
             logging.info("train test split initiated")
@@ -51,7 +55,9 @@ if __name__=="__main__":
     train_data,test_data=obj.initiate_data_ingestion()
 
     data_transformation=DataTransformation()
-    data_transformation.initiate_data_transformation(train_data,test_data)
-
+    train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data,test_data)
+   
+    modelTrainer=ModelTrainer()
+    print(modelTrainer.initiate_model_trainer(train_arr,test_arr))
 
 
